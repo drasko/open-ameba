eeb7f808ae0fe8c1a14d3ad09903644438d04045
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2017-05-14 18:47:13 +0200

    Change SDK dir name. Use OpenOCD only.
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- sdk/flasher.mk
+++ sdk/flasher.mk
@@ -116,64 +116,6 @@ _endgenbin:
 	@echo "Image ($(FLASH_IMAGE)) size $(shell printf '%d\n' $$(( $$(stat --printf="%s" $(FLASH_IMAGE)) )) ) bytes"
 	@echo "==========================================================="  
 
-ifeq ($(FLASHER_TYPE), Jlink)
-
-reset:
-	@$(JLINK_PATH)$(JLINK_EXE) -Device CORTEX-M3 -If SWD -Speed 1000 $(FLASHER_PATH)RTL_Reset.JLinkScript
-
-runram:
-	@$(JLINK_PATH)$(JLINK_EXE) -Device CORTEX-M3 -If SWD -Speed 1000 $(FLASHER_PATH)RTL_RunRAM.JLinkScript
-
-readfullflash:
-	@$(JLINK_PATH)$(JLINK_EXE) -Device CORTEX-M3 -If SWD -Speed 1000 $(FLASHER_PATH)RTL_FFlash.JLinkScript
-	
-
-flashburn:
-	@echo define call1>$(FLASHER_PATH)flash_file.jlink
-	@echo SetFirwareSize build/bin/ram_all.bin>>$(FLASHER_PATH)flash_file.jlink
-	@echo end>>$(FLASHER_PATH)flash_file.jlink
-	@echo define call2>>$(FLASHER_PATH)flash_file.jlink
-	@echo FlasherWrite build/bin/ram_all.bin 0 '$$'Image1Size>>$(FLASHER_PATH)flash_file.jlink
-	@echo end>>$(FLASHER_PATH)flash_file.jlink
-	@echo define call3>>$(FLASHER_PATH)flash_file.jlink
-	@echo FlasherWrite build/bin/ram_all.bin '$$'Image2Addr '$$'Image2Size>>$(FLASHER_PATH)flash_file.jlink
-	@echo end>>$(FLASHER_PATH)flash_file.jlink
-	@cmd /K start $(JLINK_PATH)$(JLINK_GDBSRV) -device Cortex-M3 -if SWD -ir -endian little -speed 1000 
-	@$(GDB) -x $(FLASHER_PATH)gdb_wrflash.jlink
-	#@taskkill /F /IM $(JLINK_GDBSRV)
-
-flashwebfs:
-	@echo define call1>$(FLASHER_PATH)file_info.jlink
-	@echo set '$$'ImageSize = $(shell printf '0x%X\n' $$(stat --printf="%s" $(BIN_DIR)/WEBFiles.bin))>>$(FLASHER_PATH)file_info.jlink
-	@echo set '$$'ImageAddr = 0x0D0000>>$(FLASHER_PATH)file_info.jlink
-	@echo end>>$(FLASHER_PATH)file_info.jlink
-	@echo define call2>>$(FLASHER_PATH)file_info.jlink
-	@echo FlasherWrite $(BIN_DIR)/WEBFiles.bin '$$'ImageAddr '$$'ImageSize>>$(FLASHER_PATH)file_info.jlink
-	@echo end>>$(FLASHER_PATH)file_info.jlink
-	@cmd /K start $(JLINK_PATH)$(JLINK_GDBSRV) -device Cortex-M3 -if SWD -ir -endian little -speed 1000 
-	@$(GDB) -x $(FLASHER_PATH)gdb_wrfile.jlink
-	#@taskkill /F /IM $(JLINK_GDBSRV)
-
-flashespfs:
-	@echo define call1>$(FLASHER_PATH)file_info.jlink
-	@echo set '$$'ImageSize = $(shell printf '0x%X\n' $$(stat --printf="%s" $(BIN_DIR)/webpages.espfs))>>$(FLASHER_PATH)file_info.jlink
-	@echo set '$$'ImageAddr = 0x0D0000>>$(FLASHER_PATH)file_info.jlink
-	@echo end>>$(FLASHER_PATH)file_info.jlink
-	@echo define call2>>$(FLASHER_PATH)file_info.jlink
-	@echo FlasherWrite $(BIN_DIR)/webpages.espfs '$$'ImageAddr '$$'ImageSize>>$(FLASHER_PATH)file_info.jlink
-	@echo end>>$(FLASHER_PATH)file_info.jlink
-	@cmd /K start $(JLINK_PATH)$(JLINK_GDBSRV) -device Cortex-M3 -if SWD -ir -endian little -speed 1000 
-	@$(GDB) -x $(FLASHER_PATH)gdb_wrfile.jlink
-	#@taskkill /F /IM $(JLINK_GDBSRV)
-
-
-flash_OTA:
-	@cmd /K start $(JLINK_PATH)$(JLINK_GDBSRV) -device Cortex-M3 -if SWD -ir -endian little -speed 1000 
-	@$(GDB) -x $(FLASHER_PATH)gdb_ota.jlink
-	#@taskkill /F /IM $(JLINK_GDBSRV)
-
-else
-
 flashburn:
 	@$(OPENOCD) -f interface/$(FLASHER).cfg -c "transport select swd" -f $(FLASHER_PATH)rtl8710.ocd -c "init" -c "adapter_khz $(FLASHER_SPEED)" -c "reset halt" \
 	-c "rtl8710_flash_auto_erase 1" -c "rtl8710_flash_auto_verify 1" \
@@ -200,21 +142,15 @@ flashespfs:
 	-c "rtl8710_reboot" -c "reset run" -c shutdown
 	
 reset:
-#	@$(JLINK_PATH)$(JLINK_EXE) -Device CORTEX-M3 -If SWD -Speed $(FLASHER_SPEED) flasher/RTLreset.JLinkScript
 	@$(OPENOCD) -f interface/$(FLASHER).cfg -c "transport select swd" -f $(FLASHER_PATH)rtl8710.ocd -c "init" -c "adapter_khz $(FLASHER_SPEED)" -c "reset halt" \
 	-c "rtl8710_reboot" -c shutdown
 
 runram:
-#	@$(JLINK_PATH)$(JLINK_GDB) -device Cortex-M3 -if SWD -ir -endian little -speed $(FLASHER_SPEED)
-#	@$(GDB) -x flasher/gdb_run_ram.jlink
-#	@taskkill.exe -F -IM $(JLINK_GDB)
 	@$(OPENOCD) -f interface/$(FLASHER).cfg -c "transport select swd" -f $(FLASHER_PATH)rtl8710.ocd -c "init" -c "adapter_khz $(FLASHER_SPEED)" -c "reset halt" \
 	-c "load_image $(RAM1R_IMAGE) 0x10000bc8 bin" \
 	-c "load_image $(RAM2_IMAGE) 0x10006000 bin" \
 	-c "mww 0x40000210 0x20200113" \
 	-c "reset run" -c shutdown
-
-endif
 
 $(NMAPFILE): $(ELFFILE)
 	@echo "==========================================================="
